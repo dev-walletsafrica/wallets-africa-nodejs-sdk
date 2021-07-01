687bb0b7155a489e52a1dba1116d0bf6808ac6bd
dev-walletsafrica <developers@wallet.ng>
2021-07-01 13:28:57 +0100

    update package version and link to repo

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ import Airtime from './resources/airtime';
 
 /**
  * @class WalletAfrica
- * @author sheghun {@link https://github.com/sheghun}
- * {@link https://github.com/Alexjake660/wallets-africa-nodejs-sdk}
+ * @author dev-walletsafrica {@link https://github.com/dev-walletsafrica}
+ * {@link https://github.com/dev-walletsafrica/wallets-africa-nodejs-sdk}
  *
  * Wallets Africa wrapper for communicating with the wallets africa api
  */
